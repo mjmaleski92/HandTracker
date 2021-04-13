44211d03aae604d097d6664e533f81f62b099c89
Mike Maleski <mjmaleski92@gmail.com>
2021-04-12 21:48:36 -0500

    added number recognition, 6-9 needs work still

--- hand_tracking.py
+++ hand_tracking.py
@@ -9,16 +9,16 @@ from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 
 
-# def volumeManipulator():
-devices = AudioUtilities.GetSpeakers()
-interface = devices.Activate(
-    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
-volume = cast(interface, POINTER(IAudioEndpointVolume))
-# volume.GetMute()
-# volume.GetMasterVolumeLevel()
-volumeRange = volume.GetVolumeRange()
-minVolume = volumeRange[0]
-maxVolume = volumeRange[1]
+# # def volumeManipulator():
+# devices = AudioUtilities.GetSpeakers()
+# interface = devices.Activate(
+#     IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+# volume = cast(interface, POINTER(IAudioEndpointVolume))
+# # volume.GetMute()
+# # volume.GetMasterVolumeLevel()
+# volumeRange = volume.GetVolumeRange()
+# minVolume = volumeRange[0]
+# maxVolume = volumeRange[1]
 
 
 
@@ -30,28 +30,70 @@ while True:
     success, img = cap.read()
     img = detector.findHands(img)
     lmList = detector.findPosition(img, draw=False)
+    thumbUp=True
+    indexUp=True
+    middleUp=True
+    ringUp=True
+    pinkyUp=True
     #volumeManipulator()
     if len(lmList) != 0:
+        thumbKnucklex,thumbKnuckleY=lmList[2][1], lmList[2][2]
+        indexKnuckleX,indexKnuckleY = lmList[6][1],lmList[6][2]
+        middleKnuckleX,middleKnuckleY=lmList[10][1],lmList[10][2]
+        ringKnuckleX,ringKnuckleY=lmList[14][1],lmList[14][2]
+        pinkyKnuckleX,pinkyKnuckleY=lmList[18][1],lmList[18][2]
+        
         x1, y1 = lmList[4][1], lmList[4][2]  # get location of thumb
 
         x2, y2 = lmList[8][1], lmList[8][2]  # get location of index finger
-        # find midpoint between the two fingers
-        cx, cy = (x1+x2)//2, (y1+y2)//2
 
-        cv2.line(img, (x1, y1), (x2, y2), (255, 255, 0), 3)
-        cv2.circle(img, (cx, cy), 10, (255, 30, 50), cv2.FILLED)
+        x3,y3=lmList[12][1],lmList[12][2] #get middle finger location
 
-        length = math.hypot(x2-x1, y2-y1)
-        if length < 30:
-            cv2.circle(img, (cx, cy), 10, (0, 255, 0), cv2.FILLED)
-        print(length)
+        x4,y4=lmList[16][1],lmList[16][2] #get ring finger location
 
-        # hand range 200 to 30
-        # volume range is -65.25 - 0
+        x5,y5=lmList[20][1],lmList[20][2] #get pinky location
 
-        vol = np.interp(length, [30, 175], [minVolume, maxVolume])
-        volume.SetMasterVolumeLevel(vol, None)
-        print(vol)
+        if thumbKnucklex > x1:
+            thumbUp=False
+
+        if indexKnuckleY < y2:
+            indexUp=False
+
+        if middleKnuckleY <y3:
+            middleUp=False
+
+        if ringKnuckleY < y4:
+            ringUp=False
+
+        if pinkyKnuckleY <y5:
+            pinkyUp=False
+
+        if thumbUp and indexUp and middleUp and ringUp and pinkyUp: #find 5 fingers
+            cv2.putText(img,str(5),(lmList[0][1],lmList[0][2]),cv2.FONT_ITALIC,3,(0,0,0),5)
+
+        if indexUp and middleUp and ringUp and pinkyUp and not thumbUp:
+            cv2.putText(img,str(4),(lmList[0][1],lmList[0][2]),cv2.FONT_ITALIC,3,(0,0,0),5)
+
+        if indexUp and middleUp and not ringUp and thumbUp and not pinkyUp:
+            cv2.putText(img,str(3),(lmList[0][1],lmList[0][2]),cv2.FONT_ITALIC,3,(0,0,0),5)
+
+        if indexUp and middleUp and not ringUp  and not thumbUp and not pinkyUp :
+            cv2.putText(img,str(2),(lmList[0][1],lmList[0][2]),cv2.FONT_ITALIC,3,(0,0,0),5)
+
+        if indexUp and not middleUp and not ringUp  and not thumbUp and not pinkyUp :
+            cv2.putText(img,str(1),(lmList[0][1],lmList[0][2]),cv2.FONT_ITALIC,3,(0,0,0),5)
+
+        if indexUp and middleUp and ringUp  and  thumbUp and not pinkyUp :
+            cv2.putText(img,str(6),(lmList[0][1],lmList[0][2]),cv2.FONT_ITALIC,3,(0,0,0),5)
+
+        if indexUp and middleUp and not ringUp  and  thumbUp and pinkyUp :
+            cv2.putText(img,str(7),(lmList[0][1],lmList[0][2]),cv2.FONT_ITALIC,3,(0,0,0),5)
+        
+        if indexUp and not middleUp and ringUp  and  thumbUp and  pinkyUp :
+            cv2.putText(img,str(8),(lmList[0][1],lmList[0][2]),cv2.FONT_ITALIC,3,(0,0,0),5)
+
+        if not indexUp and middleUp and ringUp  and  thumbUp and pinkyUp :
+            cv2.putText(img,str(9),(lmList[0][1],lmList[0][2]),cv2.FONT_ITALIC,3,(0,0,0),5)
 
     cTime = time.time()
     fps = 1/(cTime-pTime)
